a05319a4e0419265623bed90735d3337dd3bd87a
zero060699 <builoc191999@gmail.com>
2021-10-11 08:57:27 +0700

    crud_API

--- todo_drf/api/views.py
+++ todo_drf/api/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
 # Create your views here.
+
+def apiOverView(request):
+    return JsonResponse("API BASE POINT", safe=False)
